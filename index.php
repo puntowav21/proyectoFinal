fab5daaa9bf7df2ddb02688d9ceb7e285653bf60
21KLeo21 <leonardogrijalva210805@gmail.com>
2025-11-20 22:03:01 -0600

    materialize

--- index.php
+++ index.php
@@ -3,7 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Proyecto Final</title>
+
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <body>
     <h1>Login</h1>
--- logica/conexion.php
+++ logica/conexion.php
@@ -1,21 +1,21 @@
 <?php
-/*
+
 $host = 'localhost:3306';
 $user = 'root';
 $pass = 'l252112';
 $db = 'login_db';
-*/
+
 
 
 //cambio de base de datos 
 //hosting
-
+/*
     $host = 'sql110.infinityfree.com';
     $user = 'if0_40470405';  
     $password = 'jHV5UE9wCEfv';
     $dbname = 'if0_40470405_proyecto';
+*/
 
-    
 $conn = new mysqli($host,$user,$password,$dbname);
 
     if($conn -> connect_error){
