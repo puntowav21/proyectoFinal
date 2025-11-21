2895da3ec40d3a57031278a7a6ebac7bc55e1f9a
21KLeo21 <leonardogrijalva210805@gmail.com>
2025-11-20 21:48:37 -0600

    esqueletoInfinity

--- logica/conexion.php
+++ logica/conexion.php
@@ -1,10 +1,22 @@
 <?php
+/*
 $host = 'localhost:3306';
 $user = 'root';
 $pass = 'l252112';
 $db = 'login_db';
+*/
 
-$conn = new mysqli($host,$user,$pass,$db);
+
+//cambio de base de datos 
+//hosting
+
+    $host = 'sql110.infinityfree.com';
+    $user = 'if0_40470405';  
+    $password = 'jHV5UE9wCEfv';
+    $dbname = 'if0_40470405_proyecto';
+
+    
+$conn = new mysqli($host,$user,$password,$dbname);
 
     if($conn -> connect_error){
         die('conexion con error'. $con->connect_error);
